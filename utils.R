8bce3956ff53aa97c5878477cd22c73d74a09a28
bendeivide <ben.deivide@gmail.com>
2021-03-16 12:57:32 -0300

    Update 16/03/2021

---
+++ utils.R
@@ -0,0 +1,11 @@
+# Converter .Rmd para .md (YAML)
+
+# title: "README"
+# author: "Ben Deivide"
+# date: "16/03/2021"
+# output: 
+#   md_document:
+#   variant: markdown_github
+
+# Converter .Rmd -> .Md README.md
+usethis::use_readme_rmd()
